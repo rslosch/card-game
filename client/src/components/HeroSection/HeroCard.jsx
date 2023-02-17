0f7f1ee8d71280dfb296dbb02390136a18529ea6
Steven <slosch99@gmail.com>
2023-02-17 16:39:19 -0600

    Change x and y positions based on screen size (more mobile friendly)

--- client/src/components/HeroSection/HeroCard.jsx
+++ client/src/components/HeroSection/HeroCard.jsx
@@ -2,52 +2,25 @@ import React, { useState, useContext } from 'react'
 import Card from '../Card'
 
 import { CardsContext } from '../../context/cardsContext'
+import { ScreenSizeContext } from '../../context/screenSizeContext'
 
 import { motion } from "framer-motion"
 
-const cards = [
-    {
-      id: 1,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a cursus diam, pretium placerat ligula. Vivamus varius sodales maximus. Maecenas.",
-    },
-    {
-      id: 2,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet libero imperdiet, facilisis eros eget, placerat eros. Sed non.",
-      
-    },
-    {
-      id: 3,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet commodo mauris. Duis urna massa, congue eget tincidunt auctor.",
-    },
-    {
-      id: 4,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque arcu orci, pharetra ut urna sed, euismod sagittis nunc. Nam tristique.",
-    },
-    {
-      id: 5,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra nulla non hendrerit rutrum. Nulla in pretium mi. Maecenas dictum.",
-    },
-    {
-        id: 6,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum a lorem eget lacinia. Nulla in neque fermentum elit sagittis.",
-      }
-]
-
 const HeroCard = ({ period }) => {
   const { randomHeroCards, mainHeroCard } = useContext(CardsContext)
+  const { isSmallScreen } = useContext(ScreenSizeContext)
 
 
     const mainHeroCardVariant = {
         hidden: {
             opacity: 0,
-            // zIndex: 100,
             y: -window.innerHeight
         },
         visible: (period) => ({
             opacity: 1,
-            zIndex: 101,
+            zIndex: 100,
             y: 0,
-            scale: [3,2,1.5,1,0.6,1],
+            scale: [2.5,0.6,1],
             rotate: [0,5,15,30,20,0],
             transition:{
                 type: "spring",
@@ -63,34 +36,34 @@ const HeroCard = ({ period }) => {
     //Add conditional check if small screen or not
     const childrenInfo = [
         {
-          x: 230,
-          y: -280,
+          x: isSmallScreen ? 80 : 230,
+          y: isSmallScreen ? -190 : -280,
           rotate: -12,
         },
         {
-          x: 300, 
-          y: 0,
+          x: isSmallScreen ? 110 : 300, 
+          y: isSmallScreen ? 0 : 0,
           rotate: 22,
 
         },
         {
-          x: 350, 
-          y: 200,
+          x: isSmallScreen ? 110: 350, 
+          y: isSmallScreen ? 150: 200,
           rotate: 14,
         },
         {
-          x: -150, 
-          y: 220,
+          x: isSmallScreen ? -75 : -150, 
+          y: isSmallScreen ? 160 : 220,
           rotate: -18,
         },
         {
-          x: -400, 
-          y: -50,
+          x: isSmallScreen ? -133 : -400, 
+          y: isSmallScreen ? -35 : -50,
           rotate: -7,          
         },
         {
-          x: -150,
-          y: -200,
+          x: isSmallScreen ? -75: -150,
+          y: isSmallScreen ? -175 : -200,
           rotate: 5,
         },
     ]
@@ -121,40 +94,33 @@ const HeroCard = ({ period }) => {
         animate="visible"
         variants={mainHeroCardVariant} 
         custom={period}
-        className="
-        h-48 
-        w-36 
-        md:h-96 md:w-72 
-        relative"
-        // "z-50"
+        className="h-48 w-36 md:h-96 md:w-72 z-[101] relative"
     > 
-        <Card id={mainHeroCard.id} content={mainHeroCard.content}/>
-       { randomHeroCards.map((card, index) => (
-            <motion.div 
-                key={card.id} 
-                id={card.id} 
-                variants={childrenHeroCardVariant}
-                custom={childrenInfo[index]}
-                whileHover={{ 
-                  scale: [0.9, 1.1],
-                  rotate: [childrenInfo[index].rotate,0, childrenInfo[index].rotate],
-                  zIndex: 100
-                  // transition:{
-                  //   type: "spring",
-                  //   duration: 2,
-                  //   repeat: 1,
-                  //   repeatType: 'mirror',
-                  // }
-                }}
-                className={`h-full w-full absolute top-0 left-0 z-${cards.length-index}`}
-            >
-                <Card id={card.id} content={card.content} type={"HeroCard"}/>
-            </motion.div>
+      <Card id={mainHeroCard.id} content={mainHeroCard.content} type={"HeroCard"} subtype={"Main"}/>
+      {randomHeroCards.map((card, index) => (
+      <motion.div 
+          key={card.id} 
+          id={card.id} 
+          variants={childrenHeroCardVariant}
+          custom={childrenInfo[index]}
+          whileHover={{ 
+            scale: [0.9, 1.1],
+            rotate: [childrenInfo[index].rotate,0, childrenInfo[index].rotate],
+            zIndex: 100
+            // transition:{
+            //   type: "spring",
+            //   duration: 2,
+            //   repeat: 1,
+            //   repeatType: 'mirror',
+            // }
+          }}
+          className={`h-full w-full absolute top-0 left-0 z-${randomHeroCards.length-index}`}
+      >
+        <Card id={card.id} content={card.content} type={"HeroCard"}/>
+      </motion.div>
         ))}
     </motion.div>   
   )
-       
-    
 }
 
 export default HeroCard
