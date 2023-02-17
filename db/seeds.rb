89ec71afcaf8f0b63eb70faa59671b1de987a893
Steven <slosch99@gmail.com>
2023-02-17 11:05:09 -0600

    Add faker gem for seeds and migrate db to create Cards table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "SEEDING DATABASE..."
+
+Card.destroy_all
+
+Card.create!(
+    content: 
+        "You’re in an arena with:
+        - 50 hawks
+        - 10 crocodiles
+        - 3 brown bears
+        - 15 wolves
+        - 1 hunter with rifle
+        - 7 buffalo
+        - 10,000 rats
+        - 5 gorillas
+        - 4 lions
+        Pick 2 to defend you. The others attack you. Goal is to survive 1 hour."
+)
+Card.create!(
+    content: "Would you kill a homeless person to save 1,000 people? Nobody knows it was you who saved them and you got blood on your hands..."
+)
+Card.create!(
+    content: "If you were on a deserted island with a AI robot of the opposite sex, but it became self-aware and started coming onto you, would you fight it to the death or allow what happens to happen?"
+)
+
+10.times do
+    content = Faker::Lorem.words(number: rand(15..30)).join(' ')
+    Card.create(content: content)
+end
+
+puts "DONE SEEDING!"
