a563b70141246349a5ee66022ef72d708dd5fbba
Steven <slosch99@gmail.com>
2023-02-19 14:39:37 -0600

    Move numberPlayers state up to PlayCardGame and add randomPlayer based on numberPlayers. Add input knob styles

--- client/src/components/GameSection/PlayCardGame.jsx
+++ client/src/components/GameSection/PlayCardGame.jsx
@@ -4,41 +4,47 @@ import Slider from './Slider'
 
 import { CardsContext } from '../../context/cardsContext'
 
-// import { cards } from '../../App'
-
 import { useState } from "react"
 import { AnimatePresence } from "framer-motion"
 
 const PlayCardGame = () => {
     const [currentCard, setCurrentCard] = useState(0)
+    const [numberPlayers, setNumberPlayers] = useState(2)
+    const [randomPlayer, setRandomPlayer] = useState(null)
 
     const { cards } = useContext(CardsContext)
 
     const handleNext = () => {
       setCurrentCard((prevCard) => prevCard + 1)
       if(currentCard === cards.length - 1) setCurrentCard(0)
+      setRandomPlayer(() => getRandomPlayer(numberPlayers))
     }
   
     const currentCardIndex = cards.findIndex((card,index) => index === currentCard)
+
+    function getRandomPlayer(numberPlayers) {
+      return Math.floor(Math.random() * numberPlayers) + 1
+    }
   
-    return (
-      <div className="flex flex-col justify-center items-center h-full w-full md:h-screen md:w-screen dark:bg-black">
-        <AnimatePresence mode='wait'>
-        {currentCardIndex !== -1 && (
-            <GameCard
-              key={cards[currentCardIndex].id}
-              content={cards[currentCardIndex].content}
-            />
-        )}
-        </AnimatePresence>
-        <div className='flex flex-col justify-center items-center py-2 w-1/2'>
-          <button onClick={handleNext} className="bg-black dark:bg-white text-white dark:text-black border border-white dark:border-black w-32 py-2 px-4 rounded my-4">
-              SHUFFLE
-          </button>
-          <Slider />
-        </div>
-    </div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full md:h-screen md:w-screen dark:bg-black">
+      <AnimatePresence mode='wait'>
+      {currentCardIndex !== -1 && (
+          <GameCard
+            key={cards[currentCardIndex].id}
+            content={cards[currentCardIndex].content}
+          />
+      )}
+      </AnimatePresence>
+      <div className='flex flex-col justify-center items-center py-2 w-1/2'>
+        <p className="text-gray-700 dark:text-white text-sm font-bold">PLAYER <strong>{randomPlayer}</strong> STARTS </p>
+        <button onClick={handleNext} className="bg-black dark:bg-white text-white dark:text-black border border-white dark:border-black w-32 py-2 px-4 rounded">
+            SHUFFLE
+        </button>
+        <Slider value={numberPlayers} setValue={setNumberPlayers} />
+      </div>
+  </div>
+  )
 }
 
 export default PlayCardGame
--- client/src/components/GameSection/Slider.jsx
+++ client/src/components/GameSection/Slider.jsx
@@ -1,44 +1,56 @@
 import { useState, useEffect } from "react";
 
-const Slider = () => {
-  const [value, setValue] = useState(2)
-
+const Slider = ({value, setValue}) => {
+  const min = 2
+  const max = 10
+  const [lastKeyCode, setLastKeyCode] = useState(null)
+  
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.code === 'ArrowLeft') {
-        setValue((prevValue) => Math.max(parseInt(prevValue, 10) - 1, 2))
-      } else if (event.code === 'ArrowRight') {
-        setValue((prevValue) => Math.min(parseInt(prevValue, 10) + 1, 10))
+        if (value === min) return
+        setValue((prevValue) => Math.max(parseInt(prevValue, 10) - 1, 1))
+        setLastKeyCode('ArrowLeft')
+      } 
+      else if (event.code === 'ArrowRight') {
+        if (value < max) {
+          if (lastKeyCode === 'ArrowLeft') {
+            setValue((prevValue) => Math.min(parseInt(prevValue, 10) + 1, max))
+          } else {
+            setValue((prevValue) => Math.min(parseInt(prevValue, 10) + 1, max))
+          }
+          setLastKeyCode('ArrowRight')
+        }
       }
     }
-
+  
     document.addEventListener('keydown', handleKeyDown)
-
+  
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
-  }, [])
+  }, [value, lastKeyCode])
 
   return (
-      <div className="flex flex-col w-96 bg-white rounded-lg px-6 py-4">
+      <div className="flex flex-col w-96 bg-white dark:bg-black rounded-lg px-6 py-4">
         <div className="flex w-full justify-center">
-          <label className="block text-gray-700 text-sm font-bold mb-2"> {value} PLAYERS SELECTED </label>
+          <label className="block text-gray-700 dark:text-white text-sm font-bold mb-3"> {value} PLAYERS SELECTED </label>
         </div>
-        <div className="mb-4">
+        <div className="mb-4 w-full">
           <div className="slider relative h-1 rounded-md bg-gray-300">
             <div
               className="progress absolute h-1 bg-green-300 rounded "
             ></div>
           </div>
-          <div className="range-input relative  ">
+          <div className="range-input relative">
             <input
               onChange={(e) => {setValue(e.target.value)}}
               type="range"
-              min="2"
-              max="10"
+              min={min}
+              max={max}
               step="1"
               value={value}
-              className="range-min absolute w-full -top-1 h-1 bg-transparent appearance-none pointer-events-none"
+              className="range-min outline-none absolute w-full -top-1 h-1 bg-transparent appearance-none pointer-events-none"
             />
           </div>
         </div>
