a1559a3f08848acf144be3795b48cdaf39b17cd7
Steven <slosch99@gmail.com>
2023-02-23 17:53:59 -0600

    Mobile friendly

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-4">
-      <div className="container mx-auto flex justify-center items-center">
-        <p className="uppercase text-sm mr-2">© 2023 Cards for Insanity. All rights undeserved.</p>
+    <footer className="w-full bg-black text-white py-4 px-4">
+      <div className="mx-auto flex justify-center items-center">
+        <p className="uppercase text-xs md:text-sm mr-2">© 2023 Cards for Insanity. All rights undeserved.</p>
         <div className="flex items-center">
           <FaTwitter className="text-white mr-2" />
-          <p className="uppercase text-sm">Follow us on Twitter?</p>
+          <p className="uppercase text-xs md:text-sm">Follow us on Twitter?</p>
         </div>
       </div>
     </footer>
--- client/src/components/PostSuggestedCards.jsx
+++ client/src/components/PostSuggestedCards.jsx
@@ -4,7 +4,7 @@ import SuggestedCard from './SuggestedCard'
 
 const PostSuggestedCards = () => {
   return (
-    <div className='flex flex-col items-center h-full w-screen bg-white dark:bg-black uppercase'>
+    <div className='flex flex-col items-center h-full w-screen bg-white dark:bg-black uppercase p-16 md:p-4'>
       <h1 className='text-black dark:text-white pb-2'>Got Ideas? Send 'em here</h1>
       <SuggestedCard />
     </div>
